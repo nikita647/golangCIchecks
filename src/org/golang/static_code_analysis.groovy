2b65b72b6d5c3527fd8b3d4a59b1e24bfa213c98
Nikita joshi <97650667+nikita647@users.noreply.github.com>
2025-03-23 00:55:36 +0530

    Create static_code_analysis.groovy

---
+++ src/org/golang/static_code_analysis.groovy
@@ -0,0 +1,8 @@
+package org.golang
+
+def call() {
+  stage('static code analysis) { 
+    sh "mkdir -p reports"
+  sh "${HOME}/bin/golangci-lint run ./... --out-format json --timeout 5m>reports/Static-code-analysis-report.json || true"
+        }
+}
