9f8bcdc2b0913f0b2fae6517b3aa919477a6f36e
Nikita joshi <97650667+nikita647@users.noreply.github.com>
2025-03-22 23:46:02 +0530

    Create LicenceScanning.groovy

---
+++ src/org/genric/LicenceScanning.groovy
@@ -0,0 +1,13 @@
+package org.genric
+
+def call(String reportName) {
+    stage ('Licence Scanning'){
+        withCredentials([string(credentialsId: 'scripted_fosssa_api_key', variable: 'FOSSA_KEY')]) { 
+                    sh """
+                    export FOSSA_API_KEY=${FOSSA_KEY}
+                    fossa analyze
+                    fossa report --format html attribution >${reportFile}
+                    """
+            }
+    }
+}
