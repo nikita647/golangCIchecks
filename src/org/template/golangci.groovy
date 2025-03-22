458086cc458ace8d38e50365a91254ced7933fd7
Nikita joshi <97650667+nikita647@users.noreply.github.com>
2025-03-23 01:12:20 +0530

    Update golangci.groovy

--- src/org/template/golangci.groovy
+++ src/org/template/golangci.groovy
@@ -12,7 +12,7 @@ def call (String branch = 'main', String repoUrl, String gitPassword, String rep
  licenceScanning = new LicenceScanning()
  bugAnalysis = new bugAnalysis()
  StaticCodeAnalysis = new static_code_analysis()
- DependencyScanning = new DependencyScanning()
+ Dependency = new DependencyScanning()
 
 clean.call()
 clone.call(branch, repoUrl, gitPassword)
@@ -21,7 +21,7 @@ credScanning(reportName)
 licenceScanning(reportFile)
 bugAnalysis()
 StaticCodeAnalysis()
-DependencyScanning()
+Dependency()
   
 }
 
