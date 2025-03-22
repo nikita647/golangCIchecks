562b17faf558745e9373b74fb375522d7301229a
Nikita joshi <97650667+nikita647@users.noreply.github.com>
2025-03-23 00:14:52 +0530

    Update golangci.groovy

--- src/org/template/golangci.groovy
+++ src/org/template/golangci.groovy
@@ -4,18 +4,18 @@ import org.common.*
 import org.golang.*
 import org.genric.*
  
-def call (String branch = 'main', String repoUrl, String gitPassword, String reportName, String FOSSA_KEY, String reportFile) {
+def call (String branch = 'main', String repoUrl, String gitPassword, String reportName, String reportFile) {
  clean = new cleanworkspace()
  clone = new gitclone()
  compilation = new codeCompilation()
- licenceScanning = new LicenceScanning()
  credScanning = new  cred_scanning()
+ licenceScanning = new LicenceScanning()
 
 clean.call()
 clone.call(branch, repoUrl, gitPassword)
 compilation.call()
 credScanning(reportName)
-licenceScanning(FOSSA_KEY, reportFile)
+licenceScanning(reportFile)
   
 }
 
