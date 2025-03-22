18d9fecd5ecfb848b9f967f320b80facc168bf2a
Nikita joshi <97650667+nikita647@users.noreply.github.com>
2025-03-22 23:48:56 +0530

    Update LicenceScanning.groovy

--- src/org/genric/LicenceScanning.groovy
+++ src/org/genric/LicenceScanning.groovy
@@ -1,6 +1,6 @@
 package org.genric
 
-def call(String reportName) {
+def call(String FOSSA_KEY, String reportFile) {
     stage ('Licence Scanning'){
         withCredentials([string(credentialsId: 'scripted_fosssa_api_key', variable: 'FOSSA_KEY')]) { 
                     sh """
