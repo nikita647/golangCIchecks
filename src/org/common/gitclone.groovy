7332e91f310d518ed198022c45352a5daa852443
Nikita joshi <97650667+nikita647@users.noreply.github.com>
2025-03-23 14:52:43 +0530

    Update gitclone.groovy

--- src/org/common/gitclone.groovy
+++ src/org/common/gitclone.groovy
@@ -1,7 +1,9 @@
 package org.common
 def call(String branch = 'main', String repoUrl, String gitPassword='' ) {
+        stage('cloning repositories') {
         checkout([$class: 'GitSCM',
                     branches: [[name: "*/${branch}"]],
                     userRemoteConfigs: [[url: repoUrl , credentialsId: gitPassword ]]
                     ])
+        }
 }
